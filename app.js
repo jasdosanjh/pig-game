bc959788de3cc0697fcb77716921c904def07a88
Jas <jaspinder@dosanjh.co.uk>
2017-09-10 15:13:09 +0100

    Hold feature
    Added the hold and winner feature to the game.

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 var scores, roundScore, activePlayer;
 
-score = [0,0];
+scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-document.querySelector('.dice').style.display = 'none';
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
 
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle ('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
-    var dice = Math.floor(Math.random() * 6) + 1
+    var dice = Math.floor(Math.random() * 6) + 1;
     var diceDOM = document.querySelector('.dice');
 
     diceDOM.style.display = 'block';
@@ -22,15 +34,21 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
+        nextPlayer();
+    }
+});
 
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    scores[activePlayer] += roundScore;
 
-        document.querySelector('.player-0-panel').classList.toggle ('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
         document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        nextPlayer();
     }
 });
